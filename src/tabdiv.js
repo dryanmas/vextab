89f59eae27b0f66ac11d53c0497f83d436d15e39
Diandra Ryan-Mas <dryanmas@live.com>
2015-11-18 15:21:37 -0600

    changed $ variable

--- src/tabdiv.js
+++ src/tabdiv.js
@@ -3,7 +3,7 @@
  * Copyright Mohit Muthanna 2010 <mohit@muthanna.com>
  */
 
-$ = require('jquery')
+$j = require('jquery')
 Vex = require('vexflow')
 Artist = require('./artist.coffee')
 VexTab = require('./vextab.coffee')
@@ -22,50 +22,50 @@ Vex.Flow.TabDiv.prototype.init = function(sel, options) {
   this.sel = sel;
 
   // Grab code and clear tabdiv
-  this.code = $(sel).text();
-  $(sel).empty();
-  if ($(sel).css("position") == "static") {
-    $(sel).css("position", "relative");
+  this.code = $j(sel).text();
+  $j(sel).empty();
+  if ($j(sel).css("position") == "static") {
+    $j(sel).css("position", "relative");
   }
 
   // Get tabdiv properties
-  this.width = parseInt($(sel).attr("width")) || 400;
-  this.height = parseInt($(sel).attr("height")) || 200;
-  this.scale = parseFloat($(sel).attr("scale")) || 1.0;
+  this.width = parseInt($j(sel).attr("width")) || 400;
+  this.height = parseInt($j(sel).attr("height")) || 200;
+  this.scale = parseFloat($j(sel).attr("scale")) || 1.0;
 
   // If the Raphael.js sources are included, then use Raphael, else
   // resort to HTML5 Canvas.
   if (typeof (Raphael) == "undefined") {
-    this.canvas = $('<canvas></canvas>').addClass("vex-canvas");
-    $(sel).append(this.canvas);
+    this.canvas = $j('<canvas></canvas>').addClass("vex-canvas");
+    $j(sel).append(this.canvas);
     this.renderer = new Vex.Flow.Renderer(this.canvas[0],
         Vex.Flow.Renderer.Backends.CANVAS);
   } else {
-    this.canvas = $('<div></div>').addClass("vex-canvas");
-    $(sel).append(this.canvas);
+    this.canvas = $j('<div></div>').addClass("vex-canvas");
+    $j(sel).append(this.canvas);
     this.renderer = new Vex.Flow.Renderer(this.canvas[0],
         Vex.Flow.Renderer.Backends.RAPHAEL);
   }
 
-  this.ctx_sel = $(sel).find(".vex-canvas");
+  this.ctx_sel = $j(sel).find(".vex-canvas");
   this.renderer.resize(this.width, this.height);
   this.ctx = this.renderer.getContext();
   this.ctx.setBackgroundFillStyle(this.ctx_sel.css("background-color"));
   this.ctx.scale(this.scale, this.scale);
 
   // Grab editor properties
-  this.editor = $(sel).attr("editor") || "";
-  this.show_errors = $(sel).attr("show-errors") || "";
-  this.editor_width= $(sel).attr("editor_width") || this.width;
-  this.editor_height= $(sel).attr("editor_height") || 200;
+  this.editor = $j(sel).attr("editor") || "";
+  this.show_errors = $j(sel).attr("show-errors") || "";
+  this.editor_width= $j(sel).attr("editor_width") || this.width;
+  this.editor_height= $j(sel).attr("editor_height") || 200;
 
   var that = this;
   if (this.editor == "true") {
-    this.text_area = $('<textarea></textarea>').addClass("editor").
+    this.text_area = $j('<textarea></textarea>').addClass("editor").
       val(this.code);
-    this.editor_error = $('<div></div>').addClass("editor-error");
-    $(sel).append($('<p/>')).append(this.editor_error);
-    $(sel).append($('<p/>')).append(this.text_area);
+    this.editor_error = $j('<div></div>').addClass("editor-error");
+    $j(sel).append($j('<p/>')).append(this.editor_error);
+    $j(sel).append($j('<p/>')).append(this.text_area);
     this.text_area.width(this.editor_width);
     this.text_area.height(this.editor_height);
     this.text_area.keyup(function() {
@@ -80,8 +80,8 @@ Vex.Flow.TabDiv.prototype.init = function(sel, options) {
           }, 250);
     });
   } if (this.show_errors == "true") {
-    this.editor_error = $('<div></div>').addClass("editor-error");
-    $(sel).append($('<p/>')).append(this.editor_error);
+    this.editor_error = $j('<div></div>').addClass("editor-error");
+    $j(sel).append($j('<p/>')).append(this.editor_error);
   }
 
   // Initialize parser.
@@ -120,7 +120,7 @@ Vex.Flow.TabDiv.prototype.parseInternal = function() {
     if (this.editor_error) {
       this.editor_error.empty();
       this.editor_error.append(
-          $('<div></div>').addClass("text").html(
+          $j('<div></div>').addClass("text").html(
             "Sucky VexTab: " + e.message));
     }
   }
@@ -141,12 +141,12 @@ Vex.Flow.TabDiv.prototype.draw = function() {
 
 // Automatic initialization.
 Vex.Flow.TabDiv.start = function() {
-  $(Vex.Flow.TabDiv.SEL).each(function(index) {
+  $j(Vex.Flow.TabDiv.SEL).each(function(index) {
       new Vex.Flow.TabDiv(this);
   });
 }
 
-$(function() {if (Vex.Flow.TabDiv.SEL) { Vex.Flow.TabDiv.start() }});
+$j(function() {if (Vex.Flow.TabDiv.SEL) { Vex.Flow.TabDiv.start() }});
 
 module.exports = {
   Div: Vex.Flow.TabDiv,
--- tests/playground.js
+++ tests/playground.js
@@ -1,9 +1,9 @@
 // Load VexTab module.
 vextab = require("vextab");
-$ = require("jquery");
+$j = require("jquery");
 _ = require("underscore");
 
-$(function() {
+$j(function() {
   VexTab = vextab.VexTab;
   Artist = vextab.Artist;
   Renderer = vextab.Vex.Flow.Renderer;
@@ -12,7 +12,7 @@ $(function() {
   VexTab.DEBUG = false;
 
   // Create VexFlow Renderer from canvas element with id #boo
-  renderer = new Renderer($('#boo')[0], Renderer.Backends.CANVAS);
+  renderer = new Renderer($j('#boo')[0], Renderer.Backends.CANVAS);
 
   // Initialize VexTab artist and parser.
   artist = new Artist(10, 10, 600, {scale: 0.8});
@@ -22,15 +22,15 @@ $(function() {
     try {
       vextab.reset();
       artist.reset();
-      vextab.parse($("#blah").val());
+      vextab.parse($j("#blah").val());
       artist.render(renderer);
-      $("#error").text("");
+      $j("#error").text("");
     } catch (e) {
       console.log(e);
-      $("#error").html(e.message.replace(/[\n]/g, '<br/>'));
+      $j("#error").html(e.message.replace(/[\n]/g, '<br/>'));
     }
   }
 
-  $("#blah").keyup(_.throttle(render, 250));
+  $j("#blah").keyup(_.throttle(render, 250));
   render();
 });
